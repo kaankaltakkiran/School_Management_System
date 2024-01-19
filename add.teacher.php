131f2c3b5cf6a1db5639eb070c989eb970b47f10
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-01-19 18:18:16 +0300

    added teacher add page image

--- add.teacher.php
+++ add.teacher.php
@@ -1,12 +1,10 @@
 <?php
 @session_start();
-$activeTitle = "Add Teacher";
+$activeTitle = "Add Teacher User";
 require 'up.html.php';
 ?>
-<?php require 'navbar.php'?>
 <?php
-//!form submit edilmişse
-if (isset($_POST['submit'])) {
+if (isset($_POST['submit']) && isset($_FILES['form_image'])) {
     //!Hata mesajlarını göstermek için boş bir dizi
     $errors = array();
     require_once 'db.php';
@@ -20,6 +18,14 @@ if (isset($_POST['submit'])) {
     $password = $_POST['form_password'];
 /*  Şifrele hashleme */
     $password = password_hash($password, PASSWORD_DEFAULT);
+
+    //!Resim yükleme
+    $img_name = $_FILES['form_image']['name'];
+    $img_size = $_FILES['form_image']['size'];
+    $tmp_name = $_FILES['form_image']['tmp_name'];
+    $error = $_FILES['form_image']['error'];
+    // Hata kontrolü
+    $errors = array();
 
     //?Kullanıcı var mı yok mu kontrol etme
     $sql = "SELECT * FROM teachers WHERE useremail = :form_email";
@@ -35,58 +41,83 @@ if (isset($_POST['submit'])) {
         $errors[] = "This email is already registered";
 
         //!Eğer kullanıcı yoksa kaydet
-    } else {
-        $sql = "INSERT INTO teachers (username,useremail,usergender,useraddress,phonenumber,birthdate,userpassword) VALUES (:form_username,:form_email,:form_gender,:form_adress,:form_phonenumber,:form_birthdate,'$password')";
-        $SORGU = $DB->prepare($sql);
-        $SORGU->bindParam(':form_username', $name);
-        $SORGU->bindParam(':form_email', $email);
-        $SORGU->bindParam(':form_gender', $gender);
-        $SORGU->bindParam(':form_adress', $address);
-        $SORGU->bindParam(':form_phonenumber', $phoneNumber);
-        $SORGU->bindParam(':form_birthdate', $birthDate);
+    } else if ($error === 0) {
+        //!Resim boyutu kontrolü gözden geçmeli
+        if ($img_size < 0) {
+            $errors[] = "Sorry, your file is too large.";
+        } else {
+            $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+            $img_ex_lc = strtolower($img_ex);
+            //! Resim türü kontrolü
+            $allowed_exs = array("jpg", "jpeg", "png");
 
-        $SORGU->execute();
-        //!Kayıt başarılıysa login sayfasına yönlendir
-        /* header("location: login.php"); */
-        $approves[] = "Teacher User Added Successfully...";
+            if (in_array($img_ex_lc, $allowed_exs)) {
+                $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+                $img_upload_path = 'teacher_images/' . $new_img_name;
+                move_uploaded_file($tmp_name, $img_upload_path);
+
+                // Insert into Database
+                $sql = "INSERT INTO teachers (username,useremail,usergender,useraddress,phonenumber,birthdate,userpassword,userimg) VALUES (:form_username,:form_email,:form_gender,:form_adress,:form_phonenumber,:form_birthdate,'$password','$new_img_name')";
+                $SORGU = $DB->prepare($sql);
+
+                $SORGU->bindParam(':form_username', $name);
+                $SORGU->bindParam(':form_email', $email);
+                $SORGU->bindParam(':form_gender', $gender);
+                $SORGU->bindParam(':form_adress', $address);
+                $SORGU->bindParam(':form_phonenumber', $phoneNumber);
+                $SORGU->bindParam(':form_birthdate', $birthDate);
+
+                $SORGU->execute();
+                $approves[] = "Teacher User Added Successfully...";
+            } else {
+                $errors[] = "You can't upload files of this type";
+            }
+        }
+    } else {
+        /*     $errors[] = "unknown error occurred!"; */
+        $errors[] = "Image Not Selected";
     }
+
 }
 ?>
-    <div class="container">
+    <?php include 'navbar.php';?>
+  <div class="container">
   <div class="row justify-content-center mt-3">
   <div class="col-6">
 
-<form method="POST">
-<h1 class="alert alert-info text-center">Add Teacher User Form </h1>
+<form method="POST"enctype="multipart/form-data">
+<h1 class="alert alert-info text-center">Add Teacher User Form</h1>
 <?php
 //! Hata mesajlarını göster
 if (!empty($errors)) {
     foreach ($errors as $error) {
         echo '
-      <div class="container">
-  <div class="auto-close alert mt-3 text-center alert-danger" role="alert">
-  ' . $error . '
-  </div>
-  </div>
-  ';
+        <div class="container">
+    <div class="auto-close alert mt-3 text-center alert-danger" role="alert">
+    ' . $error . '
+    </div>
+    </div>
+    ';
     }
 }
+?>
+<?php
 //! Başarılı mesajlarını göster
 if (!empty($approves)) {
     foreach ($approves as $approve) {
         echo '
-    <div class="container">
-<div class="auto-close alert mt-3 text-center alert-success" role="alert">
-' . $approve . '
-</div>
-</div>
-';
+        <div class="container">
+    <div class="auto-close alert mt-3 text-center alert-success" role="alert">
+    ' . $approve . '
+    </div>
+    </div>
+    ';
     }
 }
 ?>
   <div class="form-floating mb-3">
   <input type="text"  class="form-control" value="<?php echo $_SESSION['userName'] ?>"disabled readonly>
-  <label>Added By Register Unit Name</label>
+  <label>Added By Admin Name</label>
 </div>
 <div class="form-floating mb-3">
   <input type="text"  class="form-control" name="form_username" required>
@@ -120,17 +151,22 @@ if (!empty($approves)) {
   Male
   </label>
 </div>
-<div class="form-check">
+<div class="form-check mb-3">
   <input class="form-check-input" type="radio" name="form_gender" value="F" required>
   <label class="form-check-label" >
   Female
   </label>
+</div>
+<div class="input-group mb-3">
+  <input type="file"  name='form_image' class="form-control" id="inputGroupFile02"required>
+  <label class="input-group-text" for="inputGroupFile02">Upload Teacher Image</label>
 </div>
 
                   <button type="submit" name="submit" class="btn btn-primary mt-3 ">Add Teacher User</button>
      </form>
      </div>
 </div>
+
 </div>
 <?php require 'footer.php'?>
 <?php require 'down.html.php';?>
