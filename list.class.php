4c994ab35a54aec9c6ef304046b21512cb6f09ec
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-01-23 12:36:20 +0300

    added teacher remove

--- list.class.php
+++ list.class.php
@@ -42,6 +42,21 @@ $SORGU = $DB->prepare("SELECT * FROM classes LIMIT 16");
 $SORGU->execute();
 $classes = $SORGU->fetchAll(PDO::FETCH_ASSOC);
 //echo '<pre>'; print_r($classes);
+if (isset($_GET['removeClassid'])) {
+    require 'db.php';
+    $remove_id = $_GET['removeClassid'];
+
+    $sql = "DELETE FROM classes WHERE classid = :removeClassid";
+    $SORGU = $DB->prepare($sql);
+
+    $SORGU->bindParam(':removeClassid', $remove_id);
+
+    $SORGU->execute();
+    echo "<script>
+alert('Class has been deleted. You are redirected to the Class List page...!');
+window.location.href = 'list.class.php';
+</script>";
+}
 
 foreach ($classes as $class) {
     echo "
@@ -49,9 +64,9 @@ foreach ($classes as $class) {
       <th>{$class['classid']}</th>
       <td>{$class['classname']}</td>
       <td>{$class['createdate']}</td>
-      <td><a href='update.admin.php?idClass={$class['classid']}' class='btn btn-success btn-sm'>Update</a></td>
-      <td><a href='update.admin.php?removeClassid={$class['classid']}' class='btn btn-danger btn-sm'>Delete</a></td>
-      <td><a href='update.admin.php?idClass={$class['classid']}' class='btn btn-info btn-sm'>list Students</a></td>
+      <td><a href='update.class.php?idClass={$class['classid']}' class='btn btn-success btn-sm'>Update</a></td>
+      <td><a href='list.class.php?removeClassid={$class['classid']}'onclick='return confirm(\"Are you sure you want to delete {$class['classname']}?\")' class='btn btn-danger btn-sm'>Delete</a></td>
+      <td><a href='?idClass={$class['classid']}' class='btn btn-info btn-sm'>list Students</a></td>
    </tr>
   ";
 }
