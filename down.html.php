d5f7933b9b888097027db0925fe20ebb912ecd1d
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-02-13 22:21:51 +0300

    added bootsrap 5 toast message for login page

--- down.html.php
+++ down.html.php
@@ -88,5 +88,12 @@ $(document).ready(function() {
         zeroRecords: 'Nothing found - sorry'
     } */
 </script>
+<script>
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl);
+    });
+    toastList.forEach(toast => toast.show());
+</script>
   </body>
 </html>
