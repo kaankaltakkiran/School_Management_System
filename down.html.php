b5f6bc30e5e930c4be66981998aa3c262a11411c
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-02-29 12:24:56 +0300

    bug fixed chancged password

--- down.html.php
+++ down.html.php
@@ -126,13 +126,6 @@ if (exampleModal) {
     const recipient = button.getAttribute('data-bs-whatever')
     // If necessary, you could initiate an Ajax request here
     // and then do the updating in a callback.
-
-    // Update the modal's content.
-    const modalTitle = exampleModal.querySelector('.modal-title')
-    const modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-    modalTitle.textContent = `New message to ${recipient}`
-    modalBodyInput.value = recipient
   })
 }
 </script>
